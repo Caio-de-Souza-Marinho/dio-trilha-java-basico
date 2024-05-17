002d98df70f7df50d2da0e3a4e7508b379dcc380
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2024-05-17 15:16:40 -0300

    aula operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,49 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // int soma = 34 + 34;
+        // System.out.println(soma);
+        // String nome = "LINGUAGEM" + "JAVA";
+        // System.out.println(nome);
+
+        // String concatenacao = "?";
+
+        // concatenacao = 1 + 1 + 1 + "1";
+        // System.out.println(concatenacao);
+
+        // concatenacao = 1 + "1" + 1 + 1;
+        // System.out.println(concatenacao);
+
+        // concatenacao = 1 + "1" + 1 + "1";
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1" + 1 + 1 + 1;
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1" + (1 + 1 + 1);
+        // System.out.println(concatenacao);
+
+        // OPERADOR TERNARIO
+
+        // sem operador ternario
+        // int a = 6;
+        // int b = 6;
+
+        // String resultado = "";
+
+        // if (a == b) {
+        //     resultado = "verdadeiro";
+        // } else {
+        //     resultado = "falso";
+        // }
+
+        // System.out.println(resultado);
+
+        // com operador ternario
+        // int a, b;
+        // a = 5;
+        // b = 6;
+
+        // String resultado = a==b ? "verdadeiro" : "falso";
+        // System.out.println(resultado);
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,17 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
 
-        byte idade = 123;
-        short ano = 2024;
-        int cep = 21070333;
-        long cpf = 98765432109L;
-        float pi = 3.14F;
-        double salario = 1275.33;
+        // byte idade = 123;
+        // short ano = 2024;
+        // int cep = 21070333;
+        // long cpf = 98765432109L;
+        // float pi = 3.14F;
+        // double salario = 1275.33;
 
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
+        // short numeroCurto = 1;
+        // int numeroNormal = numeroCurto;
+        // short numeroCurto2 = (short) numeroNormal;
 
-        final double VALOR_DE_PI = 3.14;
+        // final double VALOR_DE_PI = 3.14;
     }
 }
