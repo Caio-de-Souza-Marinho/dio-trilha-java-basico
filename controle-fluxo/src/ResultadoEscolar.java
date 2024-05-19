827ffe066adcf575e78c34bc687c2ad9263956eb
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2024-05-18 23:35:44 -0300

    aula condicional ternária

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,9 +1,7 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-
-        int nota = 6;
-
         // Condição composta
+        // int nota = 6;
         // if (nota >= 7) {
         // System.out.println("Aprovado");
         // } else {
@@ -11,12 +9,19 @@ public class ResultadoEscolar {
         // }
 
         // Condição encadeata
-        if (nota >= 7) {
-            System.out.println("Aprovado");
-        } else if (nota >= 5 && nota < 7) {
-            System.out.println("Recuperação");
-        } else {
-            System.out.println("Reprovado");
-        }
+        // int nota = 6;
+    //     if (nota >= 7) {
+    //         System.out.println("Aprovado");
+    //     } else if (nota >= 5 && nota < 7) {
+    //         System.out.println("Recuperação");
+    //     } else {
+    //         System.out.println("Reprovado");
+    //     }
+        
+        //Operador ternário
+        int nota = 3;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+        System.out.println(resultado);
     }
 }
