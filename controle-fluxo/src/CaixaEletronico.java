6d7cda0d0fb093097976fbeaa9ad64a5dbe7b202
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2024-05-18 23:22:33 -0300

    aula condicional composta

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,12 +1,21 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 12.0;
+        double valorSolicitado = 1.0;
 
-        if(valorSolicitado < saldo) {
+        // Condição simples
+        // if(valorSolicitado < saldo) {
+        // saldo = saldo - valorSolicitado;
+        // }
+
+        // Condição composta
+        if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
+            System.out.println("Saque de R$" + valorSolicitado + " efetuado com sucesso!");
+            System.out.println("Seu saldo atualizado é R$" + saldo);
+        } else {
+            System.out.println("ERRO! Saldo insuficiente");
+            System.out.println("Você possui R$" + saldo + " na conta.");
         }
-
-        System.out.println("O saldo é: " + saldo);
     }
 }
