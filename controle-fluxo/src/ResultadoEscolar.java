eaed3db6fcf9620c6fb9ced86a565d4396d17a39
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2024-05-18 23:29:05 -0300

    aula condicional encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,10 +1,20 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
 
-        int nota = 5;
+        int nota = 6;
 
+        // Condição composta
+        // if (nota >= 7) {
+        // System.out.println("Aprovado");
+        // } else {
+        // System.out.println("Reprovado");
+        // }
+
+        // Condição encadeata
         if (nota >= 7) {
             System.out.println("Aprovado");
+        } else if (nota >= 5 && nota < 7) {
+            System.out.println("Recuperação");
         } else {
             System.out.println("Reprovado");
         }
