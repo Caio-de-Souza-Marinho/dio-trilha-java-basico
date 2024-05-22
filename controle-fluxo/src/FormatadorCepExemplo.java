e1fcf6edd3237efc67c5df5f577e510b3e7a5e78
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2024-05-21 23:07:29 -0300

    aula tratamento de exceções - exceção customizada

---
+++ controle-fluxo/src/FormatadorCepExemplo.java
@@ -0,0 +1,19 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O CEP informado possui menos de 8 dígitos.");
+            System.out.println("Digite o CEP novamente.");
+        } 
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException {
+        if (cep.length() != 8)
+            throw new CepInvalidoException();
+
+        //simulando um cep formatado
+        return "23.765-064";
+    }
+}
